a8f05ab47363095dc3be396246ab708033298f65
vandyroyy <royadvandy@gmail.com>
2024-10-22 14:50:33 +0700

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import vue from '@astrojs/vue';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/serverless';
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+  // output: 'hybrid',
+  integrations: [vue(), tailwind()],
+});
